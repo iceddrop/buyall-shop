5ad670aa51e8e2e1971cd6cb6dde3fcffbbd9d32
iceddrop <93996532+iceddrop@users.noreply.github.com>
2023-09-30 10:13:11 +0100

    adjust

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,8 +6,8 @@ export default function Home(){
              <div className='home-div-two'>
                 <div className='home-text-div'>
                     <h2 className='home-title'>Shopping And Department Store.</h2>
-                    <p className='home-paragraph'>Shopping is a bit of a relaxing hobby for me, which is sometimes troubling for the bank balance.</p>
-                    <a className='learn-btn' href='#'>Learn More</a>
+                    <p className='home-paragraph mt-2'>Shopping is a bit of a relaxing hobby for me, which is sometimes troubling for the bank balance.</p>
+                    <a className='learn-btn mt-2' href='#'>Learn More</a>
                 </div>
                 <div className="product-stage-div">
                      <img src={stage} className='product-stage'alt='product-stage'/>
