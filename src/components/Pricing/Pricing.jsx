1669331c88607f0f4c2d5ae38ba1f62e28884300
iceddrop <93996532+iceddrop@users.noreply.github.com>
2023-11-25 06:51:20 +0100

    working on price section

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -1,19 +1,55 @@
+import "./Pricing.css"
 export default function Pricing() {
   return (
     <div>
       <h2 className="section-title">Get Up To 70% Off</h2>
-      <div>
-        <div>
-          <div>
-            <h4>Save</h4>
-            <div>
-              <p>$</p>
-              <h4>67</h4>
+      <div className="price-cards-div">
+        <div className="price-card">
+          <div className="text-div">
+            <h4 className="save">Save</h4>
+            <div className="price-div">
+              <p className="dollar-sign">$</p>
+              <h4 className="price-num">67</h4>
             </div>
-            <p>Explore Our Furniture & Home Furnishing Range</p>
+            <p className="paragraph">Explore Our Furniture & Home Furnishing Range</p>
           </div>
-          <div>
-            <img src="" alt="" />
+          <div className="img-div">
+          </div>
+        </div>
+        <div className="price-card">
+          <div className="text-div">
+            <h4 className="save">Save</h4>
+            <div className="price-div">
+              <p className="dollar-sign">$</p>
+              <h4 className="price-num">67</h4>
+            </div>
+            <p className="paragraph">Explore Our Furniture & Home Furnishing Range</p>
+          </div>
+          <div className="img-div">
+          </div>
+        </div>
+        <div className="price-card">
+          <div className="text-div">
+            <h4 className="save">Save</h4>
+            <div className="price-div">
+              <p className="dollar-sign">$</p>
+              <h4 className="price-num">67</h4>
+            </div>
+            <p className="paragraph">Explore Our Furniture & Home Furnishing Range</p>
+          </div>
+          <div className="img-div">
+          </div>
+        </div>
+        <div className="price-card">
+          <div className="text-div">
+            <h4 className="save">Save</h4>
+            <div className="price-div">
+              <p className="dollar-sign">$</p>
+              <h4 className="price-num">67</h4>
+            </div>
+            <p className="paragraph">Explore Our Furniture & Home Furnishing Range</p>
+          </div>
+          <div className="img-div">
           </div>
         </div>
       </div>
